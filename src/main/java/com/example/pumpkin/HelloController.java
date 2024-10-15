aaf83261bf33e09453fbc8757670a91503541508
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-15 12:52:37 +0200

    Change layout and controller to prepare for demo of model

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -1,26 +1,14 @@
 package com.example.pumpkin;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 
 public class HelloController {
-
-    public Button topButton;
     @FXML
-    private Label welcomeText;
+    private Label textLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    public void topButtonMouseEnter(MouseEvent mouseEvent) {
-        topButton.setText("Mouse entered");
-    }
-
-    public void topButtonMouseLeave(MouseEvent mouseEvent) {
-        topButton.setText("Mouse exited");
+    protected void transferButtonAction() {
+        textLabel.setText("Welcome to JavaFX Application!");
     }
 }
