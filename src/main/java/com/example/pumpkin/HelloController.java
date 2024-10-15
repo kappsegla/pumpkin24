593f863951562d8caa3f36d85e035d4a3ee6b707
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-15 13:32:28 +0200

    Make transferbutton work

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.pumpkin;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
-    @FXML
-    private Label textLabel;
+
+    Model model = new Model();
+
+    public Button transferButton;
+
+    public TextField textField;
+
+    public Label textLabel;
 
     @FXML
     protected void transferButtonAction() {
-        textLabel.setText("Welcome to JavaFX Application!");
+        model.setUserText(textField.getText());
+
+        textLabel.setText(model.getUserText());
+    }
+
+    public void textFieldChanged(KeyEvent inputMethodEvent) {
+        transferButton.setDisable(textField.getText().isEmpty());
     }
 }
---
+++ src/main/java/com/example/pumpkin/Model.java
@@ -0,0 +1,13 @@
+package com.example.pumpkin;
+
+public class Model {
+    private String userText;
+
+    public void setUserText(String text) {
+        this.userText = text;
+    }
+
+    public String getUserText() {
+        return this.userText;
+    }
+}
