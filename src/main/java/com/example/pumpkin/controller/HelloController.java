f84ead95cc42e601d0095d455765231c28d21183
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 14:34:36 +0200

    Add calls to model

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.pumpkin.controller;
 
+import com.example.pumpkin.Pumpkin;
 import com.example.pumpkin.model.Model;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -19,14 +20,14 @@ public class HelloController {
 
 
     public void image1Clicked(MouseEvent mouseEvent) {
-
+        model.pumpkinSmashed(Pumpkin.FIRST);
     }
 
     public void image2Clicked(MouseEvent mouseEvent) {
-
+        model.pumpkinSmashed(Pumpkin.SECOND);
     }
 
     public void image3Clicked(MouseEvent mouseEvent) {
-
+        model.pumpkinSmashed(Pumpkin.THIRD);
     }
 }
