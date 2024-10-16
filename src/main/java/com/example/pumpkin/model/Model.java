6fef05f693bbf9ccc51528ba656eeb668c561ec7
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 13:10:14 +0200

    Clean classes for new project

--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -1,21 +1,5 @@
 package com.example.pumpkin.model;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
 public class Model {
-    private StringProperty userText = new SimpleStringProperty();
 
-    public String getUserText() {
-        return userText.get();
-    }
-
-    public StringProperty userTextProperty() {
-        return userText;
-    }
-
-    public void setUserText(String userText) {
-        if (!userText.isEmpty())
-            this.userText.set(userText);
-    }
 }
