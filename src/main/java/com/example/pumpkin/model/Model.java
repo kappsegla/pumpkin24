8eab0b239c38b03e96626da9e81002acd20f8bb8
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 15:21:09 +0200

    Show scores

--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -1,14 +1,16 @@
 package com.example.pumpkin.model;
 
-import com.example.pumpkin.HelloApplication;
 import com.example.pumpkin.Pumpkin;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.image.Image;
 
 public class Model {
 
     private int score = 0;
+    private StringProperty scoring = new SimpleStringProperty("0 poäng");
     Image noSmash;
     Image smash;
 
@@ -16,12 +18,24 @@ public class Model {
     ObjectProperty<Image> secondPumpkin;
     ObjectProperty<Image> thirdPumpkin;
 
-    public Model(){
+    public Model() {
         noSmash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin_blank.png").toExternalForm());
         smash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin1.png").toExternalForm());
-        firstPumpkin = new SimpleObjectProperty<>(smash);
+        firstPumpkin = new SimpleObjectProperty<>(noSmash);
         secondPumpkin = new SimpleObjectProperty<>(noSmash);
         thirdPumpkin = new SimpleObjectProperty<>(noSmash);
+    }
+
+    public String getScoring() {
+        return scoring.get();
+    }
+
+    public StringProperty scoringProperty() {
+        return scoring;
+    }
+
+    public void setScoring(String scoring) {
+        this.scoring.set(scoring);
     }
 
     public Image getFirstPumpkin() {
@@ -63,8 +77,16 @@ public class Model {
     public void pumpkinSmashed(Pumpkin pumpkin) {
         //Check if we can smack this pumpkin
         //Increase score
-
-
-
+        if (pumpkin == Pumpkin.FIRST && getFirstPumpkin() == smash) {
+            setFirstPumpkin(noSmash);
+            score++;
+        } else if (pumpkin == Pumpkin.SECOND && getSecondPumpkin() == smash) {
+            setSecondPumpkin(noSmash);
+            score++;
+        } else if (pumpkin == Pumpkin.THIRD && getThirdPumpkin() == smash) {
+            setThirdPumpkin(noSmash);
+            score++;
+        }
+        setScoring(score + " poäng");
     }
 }
