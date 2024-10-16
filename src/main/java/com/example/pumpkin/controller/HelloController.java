55d147a41abaf90ac8ebb83823ba55e3c0a34f8b
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 13:47:13 +0200

    Add events for clicking on pumpkin

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.pumpkin.controller;
 
 import com.example.pumpkin.model.Model;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
 
+
+    public ImageView image1;
     //This method will run after the controller object is created
     //and the fields are initialized.
     public void initialize() {
@@ -13,4 +17,15 @@ public class HelloController {
     private Model model = new Model();
 
 
+    public void image1Clicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void image2Clicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void image3Clicked(MouseEvent mouseEvent) {
+
+    }
 }
