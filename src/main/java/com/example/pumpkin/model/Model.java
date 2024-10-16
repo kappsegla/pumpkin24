991218988c3dde26eb98aa3dd4f93cf573a603ca
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 14:34:49 +0200

    Add Images for pumpkins

--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -1,5 +1,38 @@
 package com.example.pumpkin.model;
 
+import com.example.pumpkin.HelloApplication;
+import com.example.pumpkin.Pumpkin;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.image.Image;
+
 public class Model {
 
+    private int score = 0;
+    Image noSmash;
+    Image smash;
+
+    ObjectProperty<Image> firstPumpkin;
+    ObjectProperty<Image> secondPumpkin;
+    ObjectProperty<Image> thirdPumpkin;
+
+    public Model(){
+        noSmash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin_blank.png").toExternalForm());
+        smash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin1.png").toExternalForm());
+        firstPumpkin = new SimpleObjectProperty<>(noSmash);
+        secondPumpkin = new SimpleObjectProperty<>(noSmash);
+        thirdPumpkin = new SimpleObjectProperty<>(noSmash);
+    }
+
+
+
+
+
+    public void pumpkinSmashed(Pumpkin pumpkin) {
+        //Check if we can smack this pumpkin
+        //Increase score
+
+
+
+    }
 }
