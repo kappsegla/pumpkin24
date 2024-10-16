0e0e55c2d27c2eb4e72f4dd18a4d29bb931b1489
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-16 15:40:39 +0200

    Add timeline with random duration

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -2,8 +2,13 @@ package com.example.pumpkin.controller;
 
 import com.example.pumpkin.Pumpkin;
 import com.example.pumpkin.model.Model;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
 
 import static com.example.pumpkin.Pumpkin.*;
 
@@ -15,7 +20,20 @@ public class HelloController {
     //This method will run after the controller object is created
     //and the fields are initialized.
     public void initialize() {
+        startRandomEvent();
+    }
 
+    private void startRandomEvent(){
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.millis(Math.random() * 3000),
+                        (ActionEvent event) -> {
+                            model.randomlyChangeOnePumpkin();
+                            startRandomEvent();
+                        }
+                )
+        );
+        timeline.play();
     }
 
     private Model model = new Model();
