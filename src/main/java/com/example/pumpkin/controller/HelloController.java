81e0ab11c7b7a79604953818ade2da4ddccd9811
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-18 11:10:55 +0200

    Clean up for new project

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -1,36 +1,19 @@
 package com.example.pumpkin.controller;
 
 import com.example.pumpkin.model.Model;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.util.Duration;
 
 public class HelloController {
 
     //This method will run after the controller object is created
     //and the fields are initialized.
     public void initialize() {
-        textFieldTarget.textProperty().bind(model.userTextProperty());
-        //transferButton.disableProperty().bind(textField.textProperty().isEmpty());
+
     }
 
     private Model model = new Model();
-
-    public Button transferButton;
-
-    public TextField textField;
-
-    public TextField textFieldTarget;
 
     public Model getModel() {
         return model;
     }
 
-    @FXML
-    protected void transferButtonAction() {
-        model.setUserText(textField.getText());
-    }
 }
--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -4,18 +4,5 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Model {
-    private StringProperty userText = new SimpleStringProperty();
-
-    public String getUserText() {
-        return userText.get();
-    }
-
-    public StringProperty userTextProperty() {
-        return userText;
-    }
-
-    public void setUserText(String userText) {
-        if (!userText.isEmpty())
-            this.userText.set(userText);
-    }
+  
 }
