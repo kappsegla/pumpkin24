bb1d2c0c75ccb95552ba62b8e3e2d1b2abc9a327
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-23 12:20:35 +0200

    Add final to model ref

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -6,8 +6,8 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
@@ -22,7 +22,7 @@ public class HelloController {
         startRandomEvent();
     }
 
-    private void startRandomEvent(){
+    private void startRandomEvent() {
         Timeline timeline = new Timeline(
                 new KeyFrame(
                         Duration.millis(300),
@@ -46,7 +46,7 @@ public class HelloController {
         }
     }
 
-    private Model model = new Model();
+    private final Model model = new Model();
 
     public Model getModel() {
         return model;
@@ -57,18 +57,28 @@ public class HelloController {
 
         var gc = canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        //This information will come from our model!
-        gc.setFill(Color.GREEN);
-        model.getSnake().forEach(point -> {
-            gc.fillRect(point.x() - 10,point.y() - 10, 20, 20);
-        });
-        gc.setFill(Color.RED);
-        gc.fillOval(model.getApple().x() - 10,model.getApple().y() - 10, 20, 20);
+        renderSnake(gc);
+        renderApple(gc);
+        renderGameState(gc);
+    }
 
+    private void renderGameState(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
         gc.setFont(new Font(20));
 
-        if(model.getGameState() == GameState.PAUSED)
-            gc.fillText("Press Space to start playing",200,300);
+        if (model.getGameState() == GameState.PAUSED)
+            gc.fillText("Press Space to start playing", 200, 300);
+    }
+
+    private void renderSnake(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        model.getSnake().forEach(point -> {
+            gc.fillRect(point.x() - 10, point.y() - 10, 20, 20);
+        });
+    }
+
+    private void renderApple(GraphicsContext gc) {
+        gc.setFill(Color.RED);
+        gc.fillOval(model.getApple().x() - 10, model.getApple().y() - 10, 20, 20);
     }
 }
