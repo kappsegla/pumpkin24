aa8fa9def95676b4ab42ba558007175eebc0068c
Martin Blomberg <martin.blomberg@outlook.com>
2024-10-25 09:03:20 +0200

    Add code for visual chatwindow

--- src/main/java/com/example/pumpkin/controller/HelloController.java
+++ src/main/java/com/example/pumpkin/controller/HelloController.java
@@ -1,36 +1,29 @@
 package com.example.pumpkin.controller;
 
 import com.example.pumpkin.model.Model;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.util.Duration;
 
 public class HelloController {
 
-    //This method will run after the controller object is created
-    //and the fields are initialized.
-    public void initialize() {
-        textFieldTarget.textProperty().bind(model.userTextProperty());
-        //transferButton.disableProperty().bind(textField.textProperty().isEmpty());
-    }
+    public Button sendButton;
+    public TextField textField;
 
     private Model model = new Model();
 
-    public Button transferButton;
+    //This method will run after the controller object is created
+    //and the fields are initialized.
 
-    public TextField textField;
-
-    public TextField textFieldTarget;
+    public void initialize() {
+    }
 
     public Model getModel() {
         return model;
     }
 
-    @FXML
-    protected void transferButtonAction() {
-        model.setUserText(textField.getText());
+    public void sendAction(ActionEvent actionEvent) {
+        model.sendMessage();
+
     }
 }
--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -2,9 +2,20 @@ package com.example.pumpkin.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Model {
     private StringProperty userText = new SimpleStringProperty();
+    private ObservableList<String> messages = FXCollections.observableArrayList();
+
+    public ObservableList<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(ObservableList<String> messages) {
+        this.messages = messages;
+    }
 
     public String getUserText() {
         return userText.get();
@@ -17,5 +28,10 @@ public class Model {
     public void setUserText(String userText) {
         if (!userText.isEmpty())
             this.userText.set(userText);
+    }
+
+    public void sendMessage() {
+        messages.add(userText.get());
+        userText.set("");
     }
 }
